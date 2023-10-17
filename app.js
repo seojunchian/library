6c8e187d15e516515daffbc08e9753c398f18a5d
ychilo <seojunchian@gmail.com>
2023-10-17 22:37:42 +0300

    a

--- app.js
+++ app.js
@@ -13,16 +13,23 @@ const btnSave = document.getElementById("btn-save");
 
 /* ADD BUTTON - EVENT */
 btnSave.addEventListener("click", addBook);
-let clickCount = 0;
 bookRead.addEventListener("click", () => {
-    if(clickCount % 2 == 0) {
+    if(bookRead.style.backgroundColor == "#CD5C5C") {
         bookRead.style.backgroundColor = "#06a94d";
-        clickCount += 1;
-    } else if (clickCount % 2 == 1){
-        bookRead.style.backgroundColor = "#CD5C5C"; 
-        clickCount += 1;
+    } else if(bookRead.style.backgroundColor == "#06a94d") {
+        bookRead.style.backgroundColor = "#CD5C5C";
     }
 })
+
+/* ADD BOOK - CREATE */
+function addBook() {
+    if(!tableDiv.hasChildNodes()) {
+        createTHead();
+        createTBody();
+    } else {
+        createTBody();
+    }
+}
 
 /* CREATE TABLE HEAD ELEMENTS */
 let theadElement = document.createElement("thead");
@@ -38,16 +45,6 @@ function tableStyle() {
     tableElement.style.borderRadius = "10px";
 }
 
-/* ADD BOOK - CREATE */
-function addBook() {
-    if(!tableDiv.hasChildNodes()) {
-        createTHead();
-        createTBody();
-    } else {
-        createTBody();
-    }
-}
-
 function createTHead() {
     tableStyle();
 
@@ -60,81 +57,92 @@ function createTHead() {
     for(let i=1; i<=5; i++) {
         if(i==1) {
             let tdTitleElement = document.createElement("td");
+            tdTitleElement.style.borderRadius = "5px";
             tdTitleElement.style.minWidth = "60px";
             tdTitleElement.style.maxWidth = "60px";
             tdTitleElement.textContent = "Title";
             trElement.appendChild(tdTitleElement);
         } else if(i==2) {
             let tdAuthorElement = document.createElement("td");
+            tdAuthorElement.style.borderRadius = "5px";
             tdAuthorElement.style.minWidth = "60px";
             tdAuthorElement.style.maxWidth = "60px";
             tdAuthorElement.textContent = "Author";
             trElement.appendChild(tdAuthorElement);
         } else if(i==3) {
             let tdPagesElement = document.createElement("td");
+            tdPagesElement.style.borderRadius = "5px";
             tdPagesElement.style.minWidth = "60px";
             tdPagesElement.style.maxWidth = "60px";
             tdPagesElement.textContent = "Pages";
             trElement.appendChild(tdPagesElement);
         } else if(i==4) {
             let tdReadElement = document.createElement("td");
+            tdReadElement.style.borderRadius = "5px";
             tdReadElement.style.minWidth = "60px";
             tdReadElement.style.maxWidth = "60px";
             tdReadElement.textContent = "Read";
             trElement.appendChild(tdReadElement);
         } else if(i==5) {
-            let tdReadElement = document.createElement("td");
-            tdReadElement.style.minWidth = "60px";
-            tdReadElement.style.maxWidth = "60px";
-            tdReadElement.textContent = "Options";
-            trElement.appendChild(tdReadElement);
+            let tdOptionsElement = document.createElement("td");
+            tdOptionsElement.style.borderRadius = "5px";
+            tdOptionsElement.style.minWidth = "60px";
+            tdOptionsElement.style.maxWidth = "60px";
+            tdOptionsElement.textContent = "Options";
+            trElement.appendChild(tdOptionsElement);
         }
     }
 }
-
 function createTBody() {
     /* CREATE TABLE HEAD ELEMENTS */
     let tbodyElement = document.createElement("tbody");
-    let trElement = document.createElement("tr");
-
-    /* APPEND TO DOCUMENT */
     tableElement.appendChild(tbodyElement);
+    let trElement = document.createElement("tr");
+    /* APPEND TO DOCUMENT */
     tbodyElement.appendChild(trElement);
 
     /* TABLE TEXT CONTENT */
     for(let i=1; i<=5; i++) {
         if(i==1) {
             let tdTitleElement = document.createElement("td");
+            tdTitleElement.style.borderRadius = "5px";
             tdTitleElement.innerHTML = bookTitle.value;
             trElement.appendChild(tdTitleElement);
             bookTitle.value = "";
         } else if(i==2) {
             let tdAuthorElement = document.createElement("td");
+            tdAuthorElement.style.borderRadius = "5px";
             tdAuthorElement.innerHTML = bookAuthor.value;
             trElement.appendChild(tdAuthorElement);
             bookAuthor.value = "";
         } else if(i==3) {
             let tdPagesElement = document.createElement("td");
+            tdPagesElement.style.borderRadius = "5px";
             tdPagesElement.innerHTML = bookPages.value;
             trElement.appendChild(tdPagesElement);
             bookPages.value = "";
         } else if(i==4) {
             let buttonReadElement = document.createElement("button");
-            buttonReadElement.innerHTML = "have";
+            buttonReadElement.style.borderRadius = "5px";
             buttonReadElement.style.width = "100px";
             buttonReadElement.style.height = "25px";
             buttonReadElement.style.borderRadius = "5px";
             buttonReadElement.style.border = "none";
             buttonReadElement.style.cursor = "pointer";
-            if(buttonReadElement.innerHTML == "have") {
-                buttonReadElement.style.backgroundColor = "#06a94d";
-            } else {
-                buttonReadElement.style.backgroundColor = "#CD5C5C";
-            }
+            buttonReadElement.style.backgroundColor = bookRead.style.backgroundColor;
+            buttonReadElement.addEventListener("click", () => {
+                if(bookRead.style.backgroundColor == "#CD5C5C") {
+                    buttonReadElement.style.backgroundColor = "#06a94d";
+                    buttonReadElement.innerHTML = "hasn't";
+                } else if (bookRead.style.backgroundColor == "#06a94d"){
+                    buttonReadElement.style.backgroundColor = "#CD5C5C";
+                    buttonReadElement.innerHTML = "have";
+                }
+            })
             trElement.appendChild(buttonReadElement);
-            bookRead.value = "";
         } else if(i==5) {
             let tdOptionsElement = document.createElement("td");
+            tdOptionsElement.style.borderRadius = "5px";
             tdOptionsElement.textContent = "Options";
             trElement.appendChild(tdOptionsElement);
         }
