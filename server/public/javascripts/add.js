f0c12bd15c9de1b650b5ae4221d8fe4bcbad1c47
ychilo <seojunchian@gmail.com>
2024-01-27 18:47:11 +0300

    /add search place moved to header for better view new adjustments will follow

--- server/public/javascripts/add.js
+++ server/public/javascripts/add.js
@@ -4,3 +4,7 @@ document.getElementById("header-nav-home").addEventListener("click", () => {
 document.getElementById("header-nav-see").addEventListener("click", () => {
 	window.location.href = "/books";
 });
+
+$("#main-form_add-inputs-read-button").click(() => {
+	document.getElementById("main-form_add-inputs-read").checked;
+});
